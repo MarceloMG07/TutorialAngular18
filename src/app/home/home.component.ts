08d1b4d0a546943758ef9e35a9a4db7e657bae4e
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2024-06-27 22:23:40 -0300

    Fake

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  readonly baseUrl = 'https://jsonplaceholder.typicode.com/photos';
+  housingLocation: HousingLocation = {
+    albumId: 1,
+    id: 1,
+    title: "Teste",
+    url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg",
+    thumbnailUrl: "https://picsum.photos/id/231/1800/1200"
+  };
 }
